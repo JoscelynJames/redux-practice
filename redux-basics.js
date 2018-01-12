52e11e7b87e4494c77eeb7684357e58e7092f891
Joscelyn James <josce.james7@hotmail.com>
2018-01-12 14:13:36 -0700

    added dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -6,6 +6,20 @@ const initalState = {
 };
 // Reducer
 const rootReducer = (state = initalState, action) => {
+	if (action.type === 'INC_COUNTER') {
+		// never directly mutate data in here
+		return {
+			...state,
+			counter: state.counter + 1,
+		};
+	}
+
+	if (action.type === 'ADD_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + action.value,
+		};
+	}
 	return state;
 };
 
@@ -14,5 +28,11 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+// takes js object with type property
+// convention to use all uppercase 
+// can pass any other properties 
+store.dispatch({type: 'INC_COUNTER'}); 
+store.dispatch({type: 'ADD_COUNTER', value: 10}); 
+console.log(store.getState());
 
 // Subscription 
