75d0aa7107a0e6806a81ec83758a701ccd43a7b3
Joscelyn James <josce.james7@hotmail.com>
2018-01-12 14:52:16 -0700

    added onIncrementCounter to dispatch action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,6 +4,11 @@ const initalState = {
 // make sure to set default value 
 // will get a 'cant read property of ..' error if you dont 
 const reducer = (state = initalState, action) => {
+	if (action.type === 'INCREMENT') {
+		return {
+			counter: state.counter + 1,
+		}
+	}
 	return state;
 };
 
