11e1f4a7569a7bdec82df0aced10704ee749d341
Joscelyn James <josce.james7@hotmail.com>
2018-01-12 14:48:20 -0700

    added connect function and mapStateToProps

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,9 @@
 const initalState = {
 	counter: 0,
 };
-
-const reducer = (state, action) => {
+// make sure to set default value 
+// will get a 'cant read property of ..' error if you dont 
+const reducer = (state = initalState, action) => {
 	return state;
 };
 
